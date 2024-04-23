19f5862a939d86c2acff38b9e19fd2cfac41f056
Suryaleonal <114229183+Suryaleonal@users.noreply.github.com>
2024-04-23 15:15:15 +0530

    Add files via upload

---
+++ Finder.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Finder
+{
+    public partial class PropertyListings : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Your code here
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            // Handle search button click event
+            // Implement your search logic here
+        }
+    }
+}
